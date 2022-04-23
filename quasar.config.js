fe53767a870b33fead5d4b5e4927ff3f68833529
Mila76 <mila76@mila76.it>
2022-04-23 11:08:15 +0200

    split unocss config file

--- quasar.config.js
+++ quasar.config.js
@@ -9,7 +9,6 @@
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
 
 const { configure } = require("quasar/wrappers");
-const { presetAttributify, presetUno } = require("unocss");
 
 module.exports = configure(function (/* ctx */) {
   return {
@@ -73,61 +72,7 @@ module.exports = configure(function (/* ctx */) {
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
 
-      vitePlugins: [
-        [
-          "unocss/vite",
-          {
-            presets: [presetAttributify({}), presetUno()],
-            theme: {
-              colors: {},
-              size: {
-                xs: "4px",
-                sm: "8px",
-                md: "16px",
-                lg: "24px",
-                xl: "48px",
-              },
-            },
-            rules: [
-              ["no-shadow", { "box-shadow": "none" }],
-              ["q-ma-auto", { margin: "auto" }],
-              ["q-pa-auto", { padding: "auto" }],
-              [
-                /^q-m[xy]-(\d+)$/,
-                ([, d]) => ({
-                  "margin-left": `${d / 4}rem`,
-                  "margin-right": `${d / 4}rem`,
-                }),
-              ],
-              [/^q-ma-(\d+)$/, ([, d]) => ({ margin: `${d / 4}rem` })],
-              [
-                /^q-p[xy]-(\d+)$/,
-                ([, d]) => ({
-                  "padding-left": `${d / 4}rem`,
-                  "padding-right": `${d / 4}rem`,
-                }),
-              ],
-              [/^q-pa-(\d+)$/, ([, d]) => ({ padding: `${d / 4}rem` })],
-              [
-                /^q-p[xy]-(.*)$/,
-                ([, c], { theme }) => {
-                  if (theme.size[c])
-                    return {
-                      "padding-left": theme.size[c],
-                      "padding-right": theme.size[c],
-                    };
-                },
-              ],
-              [
-                /^q-pa-(.*)$/,
-                ([, c], { theme }) => {
-                  if (theme.size[c]) return { padding: theme.size[c] };
-                },
-              ],
-            ],
-          },
-        ],
-      ],
+      vitePlugins: [["unocss/vite", {}]],
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
